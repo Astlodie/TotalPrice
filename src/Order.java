75e2eb4dc40d19bca1d41dc43efd7c433cc247ab
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-02-13 19:09:55 -0500

    Create a class Order which possesses a set of Product items (array list).

---
+++ src/Order.java
@@ -0,0 +1,13 @@
+
+public class Order {
+
+	public static void main(String[] args) {
+		 int [] arr = new int [] {2, 4, 6, 8, 10};  
+	        int sum = 0; 
+	        for (int i = 0; i < arr.length; i++) {  
+	           sum = sum + arr[i];  
+	        }  
+	        System.out.println("The total price of all product items in the Order: " + sum);  
+	}
+
+}
